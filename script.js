8cd5c5b1ff33c62f8cca8c71d1f71812017625ea
sergioimn <sergioimneris@gmail.com>
2023-02-01 10:37:47 -0300

    subo codigo del TP.

---
+++ script.js
@@ -0,0 +1,12 @@
+function redireccionarL() {
+    window.open('https://www.linkedin.com', '_blank');
+} 
+function redireccionarF() {
+    window.open('https://www.facebook.com', '_blank');
+} 
+function redireccionarT() {
+    window.open('https://www.twitter.com', '_blank');
+} 
+function redireccionarI() {
+    window.open('https://www.instagram.com', '_blank');
+}
